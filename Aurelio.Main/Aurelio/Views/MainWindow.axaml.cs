7d8a57a127f37852f2992d2525b86ef8feefb53f
Yep312 <yep21@qq.com>
2025-05-04 16:04:39 +0800

    feat: 创建 Aurelio 项目基础结构
    - 新增 .editorconfig、.gitattributes、.gitignore 文件
    - 创建 Aurelio 应用程序主项目和桌面项目- 添加应用程序资源、样式和视图
    - 配置项目依赖和版本管理

---
+++ Aurelio.Main/Aurelio/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using SukiUI.Controls;
+
+namespace Aurelio.Views;
+
+public partial class MainWindow : SukiWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
